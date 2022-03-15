436ee3dda9c212985ff99bcd2210682f4fc7474e
ggoliwas <goliwas.grzegorz@gmail.com>
2022-03-15 20:26:55 +0100

    add mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-console.log('dupa');
+const hamburger = document.querySelector('.hamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.js-overlay');
+
+const handleClick = () => {
+    hamburger.classList.toggle('hamburger--active');
+    header.classList.toggle('header--active');
+    overlay.classList.toggle('js-overlay--active');
+}
+
+
+hamburger.addEventListener('click', handleClick);
